8eb2fb64f37f81bcc5535ddabee2a9343dfb0597
ValentinaZussino <vzussino@protonmail.com>
2022-10-10 17:15:11 +0200

    alcune sistemazioni layout e commenti

--- js/script.js
+++ js/script.js
@@ -4,19 +4,21 @@ let vzContainer = document.querySelector('div.row');
 for(let i = 0; i <= 100; i++){
     // creo elemento div e aggiungo classe square per tutti
     const vzSquare = document.createElement('div');
-    vzSquare.className = 'vz_square';
+    vzSquare.className = ' vz_square';
     // inserisco fizzbuzz e aggiungo classe fizzbuzz
-    if((i % 3 == 0) && (i % 5 === 0)){
-        vzSquare.append('fizzbuzz');
+    if((i % 3 === 0) && (i % 5 === 0)){
+        vzSquare.append('FizzBuzz');
         vzSquare.className += ' fizzbuzz';
     }  else if (i % 3 === 0){ // inserisco fizz e aggiungo classe fizz
-        vzSquare.append('fizz');
+        vzSquare.append('Fizz');
         vzSquare.className += ' fizz';
     } else if (i % 5 === 0){ // inserisco buzz e aggiungo classe buzz
-        vzSquare.append('buzz');
+        vzSquare.append('Buzz');
         vzSquare.className += ' buzz';
-    } else { // inserisco i
+    } else { // inserisco i e aggiungo classe vzi
         vzSquare.append(i);
+        vzSquare.className += ' vz_i';
     }
+    // aggiungo quadratini al container
     vzContainer.append(vzSquare);
 }

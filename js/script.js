cbb4200cb87fa417861f9c5dbf6d1809bde86403
ValentinaZussino <vzussino@protonmail.com>
2022-10-10 15:59:31 +0200

    cambio impostaz da div.square a container

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-// prendo il quadratino
-let square = document.querySelector('div.vz_square');
+// prendo il contenitore
+let vzContainer = document.querySelector('div.row');
 // creo ciclo
 for(let i = 0; i < 100; i++){
+    // creo elemento div
+    const vzSquare = document.createElement('div');
     // inserisco fizzbuzz e aggiungo classe square
     if((i % 3 == 0) && (i % 5 === 0)){
-        square.append('fizzbuzz');
-        square.className = 'vz_square';
+        vzSquare.append('fizzbuzz');
+        vzSquare.className = 'vz_square';
+        vzContainer.append(vzSquare);
     }  else if (i % 3 === 0){ // inserisco fizz e aggiungo classe square
-        square.append('fizz');
-        square.className = 'vz_square';
+        vzSquare.append('fizz');
+        vzSquare.className = 'vz_square';
+        vzContainer.append(vzSquare);
     } else if (i % 5 === 0){ // inserisco buzz e aggiungo classe square
-        square.append('buzz');
-        square.className = 'vz_square';
-    } else {
-        square.append(i);
-        square.className = 'vz_square';
+        vzSquare.append('buzz');
+        vzSquare.className = 'vz_square';
+        vzContainer.append(vzSquare);
+    } else { // inserisco i e aggiungo classe square
+        vzSquare.append(i);
+        vzSquare.className = 'vz_square';
+        vzContainer.append(vzSquare);
     }
 }
-// prendo la row
-const tabSquares = document.querySelector('div.row');
-// inserisco i quadratini
-tabSquares.append(square);

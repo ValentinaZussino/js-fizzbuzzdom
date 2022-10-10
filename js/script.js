3d4c23a49a8ba6bc8a36eaafa2bd333143b88749
ValentinaZussino <vzussino@protonmail.com>
2022-10-10 15:28:42 +0200

    prima prova impostazione js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert('ciao')
+// prendo il quadratino
+let square = document.querySelector('div.vz_square');
+// creo ciclo
+for(let i = 0; i < 100; i++){
+    // inserisco fizzbuzz e aggiungo classe square
+    if((i % 3 == 0) && (i % 5 === 0)){
+        square.append('fizzbuzz');
+        square.className = 'vz_square';
+    }  else if (i % 3 === 0){ // inserisco fizz e aggiungo classe square
+        square.append('fizz');
+        square.className = 'vz_square';
+    } else if (i % 5 === 0){ // inserisco buzz e aggiungo classe square
+        square.append('buzz');
+        square.className = 'vz_square';
+    } else {
+        square.append(i);
+        square.className = 'vz_square';
+    }
+}
+// prendo la row
+const tabSquares = document.querySelector('div.row');
+// inserisco i quadratini
+tabSquares.append(square);
